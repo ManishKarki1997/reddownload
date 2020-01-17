afb518cfd62e6ed079ec3e39ba1f1dfd1c95cacf
Manish Karki <manishkarki247@gmail.com>
2020-01-17 20:04:34 +0545

    preparing for global package publishing

---
+++ index.js
@@ -0,0 +1,141 @@
+#!/usr/bin/env node
+
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+const request = require('request');
+const { DownloaderHelper } = require('node-downloader-helper');
+
+
+
+// yargs, to get arguments from commandline
+const argv = require('yargs').argv;
+const path = require("path");
+const fs = require('fs');
+
+
+const baseAPI = "https://www.reddit.com/r/";
+
+let subredditName = argv.name;
+let postLimit = argv.limit || 100;
+let sortBy = argv.sort || 'new';
+let fileTitleLength = 14;
+
+const wallpaperLinks = [];
+const imgurWallpaperLinks = [];
+let directoryFiles = [];
+
+
+
+
+const fetchPosts = async () => {
+    try {
+
+        // if user didn't specify subreddit name, throw an error
+        if (argv.name === undefined) throw ("Please provide a subreddit name");
+
+        readDirectoryFiles();
+
+        const resData = await axios.get(`${baseAPI}${subredditName}/${sortBy}/.json?limit=${postLimit}`);
+
+        const posts = resData.data.data.children;
+
+        if (posts.length >= 0) {
+            // check if folder already exists, if not, create it
+            checkIfFolderExists();
+
+            posts.forEach(post => {
+                if (post.data.url.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+
+                    // If the file doesn't already exist, set it up for download!!!
+
+                    if (directoryFiles.indexOf(post.data.title.slice(0, fileTitleLength)) == -1) {
+
+                        wallpaperLinks.push({
+                            title: post.data.title,
+                            url: post.data.url
+                        });
+                    } else {
+                        console.log(`${post.data.title} is already downloaded!`)
+                    }
+
+                } else if (post.data.domain === 'imgur.com') {
+
+                    if (directoryFiles.indexOf(post.data.title.slice(0, fileTitleLength)) == -1) {
+                        imgurWallpaperLinks.push({
+                            title: post.data.title,
+                            url: post.data.url
+                        })
+                    } else {
+                        console.log(`${post.data.title} is already downloaded!`)
+
+                    }
+                }
+
+            })
+
+        } else {
+            throw ("Could not find any post in the subreddit. Did you type in correct subreddit name?");
+        }
+
+        if (imgurWallpaperLinks.length > 0) {
+            getImgurWalls();
+        }
+        downloadWallpapers();
+
+
+    } catch (error) {
+        return console.log(error)
+    }
+}
+
+const checkIfFolderExists = () => {
+    if (!fs.existsSync(path.join(__dirname, subredditName))) {
+        fs.mkdirSync(subredditName);
+    }
+}
+
+const downloadWallpapers = () => {
+    wallpaperLinks.forEach((wallpaper, index) => {
+
+        const target_url = wallpaper.url;
+        const file_name = wallpaper.title.slice(0, fileTitleLength); //full title gave some error while saving, so gonna have to slice it :(
+        // const file_path = path.join(path.join(__dirname, subredditName), file_name);
+        const dl = new DownloaderHelper(target_url, path.join(__dirname, subredditName), { fileName: file_name });
+
+        dl.on('end', () => console.log('Downloaded ' + parseInt(index + 1) + ' of ' + wallpaperLinks.length));
+
+        dl.start();
+
+
+    })
+}
+
+
+
+const getImgurWalls = () => {
+    imgurWallpaperLinks.forEach(iWalls => {
+        request(iWalls.url, (err, res, body) => {
+            if (err) return console.log(err);
+            const $ = cheerio.load(body);
+
+            const imgURL = $("link[rel='image_src']").attr('href');
+            wallpaperLinks.push({
+                title: iWalls.title,
+                url: imgURL
+            })
+        })
+    })
+
+}
+
+const readDirectoryFiles = () => {
+    checkIfFolderExists();
+    const files = fs.readdirSync(path.join(__dirname, subredditName));
+    files.forEach(file => {
+        directoryFiles.push(path.basename(file));
+    })
+}
+
+
+fetchPosts();
